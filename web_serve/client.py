023bfcdaf7a5f03c6a836461954ec7c3626baa27
zihui_xu <zihui_xu@126.com>
2017-08-25 16:04:31 +0800

    socket

--- web_serve/client.py
+++ web_serve/client.py
@@ -1,12 +1,22 @@
 
 import socket
+import sys
 
-#listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s = socket.socket()
+# 创建 socket 对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# 获取本地主机名
 host = socket.gethostname()
-port = 1234
 
+# 设置端口好
+port = 9999
+
+# 连接服务，指定主机和端口
 s.connect((host, port))
 
-print(s.recv(1024))
+# 接收小于 1024 字节的数据
+msg = s.recv(1024)
+
+s.close()
+
+print (msg.decode('utf-8'))
--- web_serve/server.py
+++ web_serve/server.py
@@ -1,24 +1,31 @@
+
 import socket
+import sys
 
-# listen_socket = socket.socket()  创建socket对象
-# listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s = socket.socket()
+# 创建 socket 对象
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# 获取本地主机名
 host = socket.gethostname()
-port = 1234
+# 设置端口号
+port = 9999
 
 # setsockopt(level, optname, value)  设置给定套接字选项的值
 #listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+# 绑定端口
 # bind() 绑定地址（host,port）到套接字， 在AF_INET下,以元组（host,port）的形式表示地址。
-s.bind((host, port))
+serversocket.bind((host, port))
 
-#等待客户端连接
-s.listen(5)
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
 
 while True:
-    #建立客户端连接
-    conn, addr = s.accept()
-    print("Got connection from", addr)
-    s.send("think you for connecting")
-    s.close()
+    # 建立客户端连接
+    clientsocket,addr = serversocket.accept()
+
+    print("连接地址: %s" % str(addr))
+
+    msg='Think you for connecting'+ "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
