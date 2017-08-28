73c0210af66550f82adcc57da5c9f1b33f730f00
xuzh <phonegapcordova@sina.com>
2017-08-28 19:26:55 +0800

    train tickets quiry

---
+++ train_quiry/tickets.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+"""Train tickets query via command-line.
+
+Usage:
+    tickets [-gdtkz] <from> <to> <date>
+
+Options:
+    -h,--help        显示帮助菜单
+    -g               高铁
+    -d               动车
+    -t               特快
+    -k               快速
+    -z               直达
+
+Example:
+    tickets 南京 北京 2016-07-01
+    tickets -dg 南京 北京 2016-07-01
+"""
+from docopt import docopt
+from stations import stations
+import requests
+
+def cli():
+    """command-line interface"""
+    arguments = docopt(__doc__)
+    # print(arguments)
+    from_station = stations.get(arguments['<from>'])
+    to_station = stations.get(arguments['<to>'])
+    date = arguments['<date>']
+    # 构建URL
+    # https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2017-08-28&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=BJP&purpose_codes=ADULT
+    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(
+        date, from_station, to_station
+    )
+    # 添加verify=False参数不验证证书
+    r = requests.get(url, verify=False)
+    print(r.json())
+
+if __name__ == '__main__':
+    cli()
