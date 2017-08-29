809e22f64ced55e1ff10fd9ee96b159ba58c28c9
Bronze-hammer <zihui_xu@126.com>
2017-08-29 11:23:35 +0800

    Update tickets.py

--- train_quiry/tickets.py
+++ train_quiry/tickets.py
@@ -1,41 +1,52 @@
-# coding: utf-8
 
-"""Train tickets query via command-line.
+
+"""命令行火车票查看器
 
 Usage:
     tickets [-gdtkz] <from> <to> <date>
 
 Options:
-    -h,--help        显示帮助菜单
-    -g               高铁
-    -d               动车
-    -t               特快
-    -k               快速
-    -z               直达
+    -h,--help   显示帮助菜单
+    -g          高铁
+    -d          动车
+    -t          特快
+    -k          快速
+    -z          直达
 
 Example:
-    tickets 南京 北京 2016-07-01
-    tickets -dg 南京 北京 2016-07-01
+    tickets 北京 上海 2016-10-10
+    tickets -dg 成都 南京 2016-10-10
 """
 from docopt import docopt
 from stations import stations
+from prettytable import PrettyTable
 import requests
+import json
+import urllib3
+urllib3.disable_warnings()
+
+
 
 def cli():
     """command-line interface"""
     arguments = docopt(__doc__)
-    # print(arguments)
     from_station = stations.get(arguments['<from>'])
     to_station = stations.get(arguments['<to>'])
     date = arguments['<date>']
     # 构建URL
-    # https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=2017-08-28&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=BJP&purpose_codes=ADULT
+    # url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate={}&from_station={}&to_station={}'.format(
+    #     date, from_station, to_station
+    # )
     url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(
         date, from_station, to_station
     )
+
     # 添加verify=False参数不验证证书
-    r = requests.get(url, verify=False);
-    print(r.json())
+    r = requests.get(url, verify=False)
+    # r.encoding = "gbk"
+    # print(r.json())
+    available_trains = r.json()['data']['result']
+    print(available_trains)
 
 if __name__ == '__main__':
     cli()
