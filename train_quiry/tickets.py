5e54c1d69da9e064777a56d3e9e2c9362549271e
Bronze-hammer <zihui_xu@126.com>
2017-08-31 16:31:09 +0800

    Update tickets.py

--- train_quiry/tickets.py
+++ train_quiry/tickets.py
@@ -1,5 +1,4 @@
 
-
 """命令行火车票查看器
 
 Usage:
@@ -25,17 +24,6 @@ import requests
 import urllib3
 urllib3.disable_warnings()
 
-# header = {
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#     'Accept-Encoding': 'gzip, deflate, br',
-#     'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-#     'Connection': 'keep-alive',
-#     'Cookie':'JSESSIONID=A9AE0B383F8F1BB150A9E99FC5D3162A; fp_ver=4.5.1; RAIL_EXPIRATION=1504288742858; RAIL_DEVICEID=G3PBmT-QYa5wK7Zx6fpMLX9kBK6lk2FaP51yEk9hsx2ykyWKabef4SWyQ8rSY1EZqOPRkMIbNojHN-Lp1pn605Qi4p-iYzNVtPLHStTmUnxnb6eF-YAif5vdy6BXj7UWRh7ost6U6se9IgW-7q6gCH1Ay-D2iSMP; _jc_save_fromStation=%u4E0A%u6D77%2CSHH; _jc_save_toStation=%u5317%u4EAC%2CBJP; _jc_save_fromDate=2017-08-30; _jc_save_toDate=2017-08-30; _jc_save_wfdc_flag=dc; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=367526154.24610.0000',
-#     'Host':'kyfw.12306.cn',
-#     'Upgrade-Insecure-Requests':'1',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
-#                   '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-# }
 
 class TrainsCollection:
     header = '车次 车站 时间 历时 一等 二等 软卧 硬卧 硬座 无座'.split()
@@ -94,10 +82,8 @@ def cli():
     from_station = stations.get(arguments['<from>'])
     to_station = stations.get(arguments['<to>'])
     date = arguments['<date>']
+    
     # 构建URL
-    # url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate={}&from_station={}&to_station={}'.format(
-    #     date, from_station, to_station
-    # )
     url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(
         date, from_station, to_station
     )
@@ -114,3 +100,6 @@ def cli():
 
 if __name__ == '__main__':
     cli()
+
+    
+    
